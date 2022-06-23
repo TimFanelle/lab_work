7504c20ed69a16e5899c537aa76eaad84ed028d6
TimFanelle <pychocow.com@gmail.com>
2022-06-23 08:24:16 -0700

    Creating a new branch for finger movement code

---
+++ NI_Server_zmq.c
@@ -0,0 +1,217 @@
+// quickDAQ.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+#include <stdio.h>
+#include <Windows.h>
+#include <quickDAQ.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include <string.h>
+
+#include <zmq.h>
+
+#define muscles 3
+#define encodes 3
+
+char buffer[1024];
+char recvBuffer[1024];
+float activations[muscles];
+float encoders[encodes];
+float potential = 0.0;
+char obs[1024];
+char* token;
+
+int option;
+
+const unsigned int PACKET_SIZE = muscles + encodes;
+
+void readEncoders(){
+    char smallBuf[20];
+    strcpy(obs, "");
+    for(int i = 0; i<encodes; i++){
+        gcvt(encoders[i], 16, smallBuf);
+        if(i>0){
+            strcat(obs, ", ");
+        }
+        strcat(obs, smallBuf);
+    }
+    gcvt(potential, 16, smallBuf);
+    strcat(obs, ", ");
+    strcat(obs, smallBuf);
+}
+
+//using namespace std;
+int main()
+{
+
+	// initialize
+	//int iterations = count * 1000; //define how many iterations will occur
+
+	srand((unsigned)time(0));
+	quickDAQinit();
+
+	// configure channels and sample clock
+	pinMode(5, ANALOG_IN, 0);
+	for (int i = 0; i < muscles; i++) {
+		pinMode(2, ANALOG_OUT, i);
+	}
+	pinMode(2, ANALOG_OUT, 16);
+	pinMode(2, ANALOG_OUT, 17);
+	pinMode(2, ANALOG_OUT, 18);
+	pinMode(2, ANALOG_OUT, 19);
+
+	pinMode(2, DIGITAL_OUT, 0);
+
+	for (int i = 0; i < encodes; i++) {
+		pinMode(3, CTR_ANGLE_IN, i);
+	}
+
+	setSampleClockTiming((samplingModes)HW_CLOCKED/*DAQmxSampleMode*/, DAQmxSamplingRate, DAQmxClockSource, (triggerModes)DAQmxTriggerEdge, DAQmxNumDataPointsPerSample, TRUE);
+
+	printf("\nIO timeout is %f\n", DAQmxDefaults.IOtimeout);
+
+	// read/write data
+	float64 AI;
+	float64 mtrCmd[16] = { 0,0,0, 0,0,0, 0,0,0, 0,0,0,	0,0,0,0 };
+	const float64 muscleTone = 0.2;
+	uInt32			DO1 = 0x000000ff;
+	const float64	DO2[4] = { 5,5,5,5 };
+	float64 CI[8] = { 0,0, 0,0, 0,0, 0,0 };
+
+	//printf("\nData to be written: AO: %lf, DO: %lX\n", (double)AO, DO);
+	printf("Press enter to start control of hand");
+	getchar();
+
+	// start tasks
+	quickDAQstart();
+
+
+	syncSampling(); //wait for HW timed sample
+
+	// Enable motor amps
+	readAnalog(5, &AI);
+	for (int i = 12; i < 16; i++) {
+		mtrCmd[i] = DO2[i - 12];
+	}
+	writeDigital(2, &DO1); // enable first 8 motor amplifiers
+	writeAnalog(2, &(mtrCmd[0])); // enable 9-12th motor amps
+	printf("Motor Enabled\n");
+
+	// provide muscle tone - ensures no tendon/cable is slack
+	for (int i = 0; i < 12; i++) {
+		mtrCmd[i] = muscleTone;
+	}
+	writeAnalog(2, &(mtrCmd[0]));
+	printf("Motor Wound up\n");
+
+    // START SERVER
+
+    /* Initializing server socket */
+    void *context = zmq_ctx_new();
+    void *data_socket = zmq_socket(context, ZMQ_REP);
+    const int rb = zmq_bind(data_socket, "tcp://*:4268");
+    assert(rb == 0);
+    
+    int q = 1;
+    while(True){
+        /* Receiving client data */
+        zmq_recv(data_socket, recvBuffer, 1024, 0);
+        
+        /* sync */
+        syncSampling(); //sync every time a connection is accepted
+        readAnalog(5, &AI);
+
+        /* break it into parts */
+        int l = 0;
+        token = strtok(recvBuffer, ";");
+        sscanf(token, "%d", &option);
+
+        /* determine action */
+        switch(option){
+            case 1:
+                /* assign muscle activations and save encoder values and slider value */
+                token = strtok(NULL, ";");
+                while(token != NULL){
+                    sscanf(token, "%f", &activations[l]);
+                    token = strtok(NULL, ";");
+                    l++;
+                }
+                printf("\n\nMOTOR:");
+                for(int i = 0; i < muscles; i++){
+                    mtrCmd[i] = activations[i] + muscleTone;
+                    printf(" %3.1f", mtrCmd[i]);
+                }
+                printf("\n\n");
+                writeAnalog(2, &(mtrCmd[0]));
+                for(int j = 0; j<encodes; j++){
+                    readCounterAngle(3, j, &(CI[j]))
+                    encoders[j] = CI[j];
+                }
+                //TODO: read potentiameter
+                readEncoders();
+                break;
+            case 2:
+                /* take the outputs from the encoders and slider and save them*/
+                for(int j = 0; j<encodes; j++){
+                    readCounterAngle(3, j, &(CI[j]))
+                    encoders[j] = CI[j];
+                }
+                //TODO: read potentiameter
+                readEncoders();
+                break;
+            case 3:
+                /* break loop, shut down server, and finish running program*/
+                zmq_send(data_socket, "Finished", 9, 0);
+                printf("Completed and closed");
+                q = 0;
+            default:
+                break;
+        }
+        /* break infinite loop */
+        if(q == 0){
+            break;
+        }
+        /* Send encoder and slider values back to client */
+        printf("%s\n", obs);
+        strcpy(buffer, obs);
+        len = strlen(obs);
+        zmq_send(data_socket, buffer, len, 0);
+
+        sleep(1);
+    }
+    const int rc = zmq_close(data_socket);
+
+    // END SERVER
+
+	syncSampling();
+	readAnalog(5, &AI);
+	for (int i = 0; i < 16; i++) {
+		mtrCmd[i] = 0;
+	}
+	writeAnalog(2, &(mtrCmd[0]));
+	printf("\n\nMotor wound down\n");
+
+	DO1 = 0x00000000;
+	writeDigital(2, &(DO1));
+	printf("Motor disabled\n\n");
+
+	syncSampling();
+
+	// end tasks
+	quickDAQstop();
+
+	// Terminate library
+	quickDAQTerminate();
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ SliderPPO.py
@@ -0,0 +1,224 @@
+# NOTE: observations may not need switch as input, that may just be for the cost function
+import torch
+import torch.nn as nn
+from gym import spaces
+
+from torch.distributions import MultivariateNormal
+from torch.optim import Adam
+from network import FeedForwardNN
+import numpy as np
+
+class singleFinger():
+    def __init__(self, motors, encoders):
+        # Define dimensions
+        mots = np.array([0]*motors)
+        mote = np.array([1]*motors)
+        self.action_space = spaces.Box(low=mots, high=mote, dtype=np.float64)
+            # I might also need to make a spaces.box for the observation space
+        self.obs_dim = encoders 
+        self.act_dim = self.action_space.shape[0]
+
+        #Initialize actor and critic networks
+        self.actor = FeedForwardNN(self.obs_dim, self.act_dim)
+        self.critic = FeedForwardNN(self.obs_dim, 1)
+
+        self.__init_hyperparameters()
+
+        #initialize optimizers
+        self.actor_optim = Adam(self.actor.parameters(), lr = self.lr)
+        self.critic_optim = Adam(self.critic.parameters(), lr = self.lr)
+
+        # Create varible for the matrix
+        self.cov_var = torch.full(size=(self.act_dim,), fill_value=0.5)
+
+        # Create the covariance matrix
+        self.cov_mat = torch.diag(self.cov_var)
+    
+    
+    def __init_hyperparameters(self):
+        self.max_timesteps_per_episode = 1000
+
+        self.gamma = 0.95
+        self.n_updates_per_iteration = 5
+        self.clip = 0.2
+        self.lr = 0.005
+
+        self.goal_volt = 1
+        self.last_volt = 0
+    
+
+    def get_cur_obs(self):
+        obs = [0] * self.obs_dim
+
+        #TODO: read all the encoder states and the slider state
+        '''
+        # this is stand-in pseudocode that will have to be changed
+        for i in range(len(encoders)):
+            obs[i] = encoders[i].readout()
+        obs[-1] = switch.state()
+        '''
+        
+        obs = np.array(obs)
+        return obs
+    
+
+    def step(self, action):
+        # take input from action space and do muscle movements
+        # TODO: write this
+        action = action.detach().numpy()
+        '''
+        # this is stand-in pseudocode that will have to be changed
+        for i in range(len(action)):
+           motors[i].write(action[i]) #this should be done for 0.01 seconds
+        '''
+
+        # get observations
+        observed = self.get_cur_obs()
+
+        # measure input from slider switch
+        voltage_inp = 0
+
+        # determine if done
+        done = bool(voltage_inp >= self.goal_volt-0.1) # subtract 0.1 to give some leeway and account for fluctuations
+
+        reward = 0
+        if done:
+            reward = 100.0
+        delta_volt = voltage_inp - self.last_volt
+        reward += (delta_volt**2) * np.sign(delta_volt)
+        return observed, reward, done
+
+
+    def rollout(self):
+        #episode data
+        ep_obs = []
+        ep_acts = []
+        ep_log_probs = []
+        ep_rews = []
+        ep_rtgs = []
+
+        # number of timesteps
+        t = 0
+        obs = self.get_cur_obs()
+        done = False
+
+        for ep_t in range(self.max_timesteps_per_episode):
+            #increment timesteps
+            t += 1
+
+            #collect observation
+            ep_obs.append(obs)
+
+            action, log_prob = self.get_action(obs)
+            obs, rew, done = self.step(action)
+
+            #collect reward, action, and log_prob
+            ep_rews.append(rew)
+            ep_acts.append(action)
+            ep_log_probs.append(log_prob)
+
+            if done:
+                break
+        t += 1 # plus 1 because timestep started at 0
+
+        #reshape data as tensors
+        ep_obs = torch.tensor(ep_obs, dtype=torch.float)
+        ep_acts = torch.tensor(ep_acts, dtype=torch.float)
+        ep_log_probs = torch.tensor(ep_log_probs, dtype=torch.float)
+
+        ep_rtgs = self.compute_rtgs(ep_rews)
+
+        #return data
+        return ep_obs, ep_acts, ep_log_probs, ep_rtgs, t
+    
+
+    def compute_rtgs(self, ep_rews):
+        ep_rtgs = []
+
+        #iterate through the episode backwardsto maintain order
+        discounted_reward = 0
+        for rews in reversed(ep_rews):
+            discounted_reward = rews + discounted_reward*self.gamma
+            ep_rews.insert(0, discounted_reward)
+        #convert rewards-to-go into a tensor
+        ep_rtgs = torch.tensor(ep_rtgs, dtype=torch.float)
+
+        return ep_rtgs
+    
+
+    def get_action(self, obs):
+        # Query the actor network for a mean action
+        mean = self.actor(obs)
+
+        # create multivariate normal distribution
+        dist = MultivariateNormal(mean, self.cov_mat)
+
+        # sample an action from the distribution and get its log probability
+        action = dist.sample()
+        action = torch.tensor(np.clip(action.detach().numpy(), self.action_space.low, self.action_space.high), dtype=torch.float)
+        log_prob = dist.log_prob(action)
+
+        # return the action and log_prob
+        return action.detach().numpy(), log_prob.detach()
+    
+
+    def evaluate(self, ep_obs, ep_acts):
+        # Query critic network for a value for each observation
+        V = self.critic(ep_obs).squeeze()
+
+        # calculate the log probabilityes of the episode actions using the most up-to-date actor network
+        mean = self.actor(ep_obs)
+        dist = MultivariateNormal(mean, self.cov_mat)
+        log_probs = dist.log_prob(ep_acts)
+
+        # return predicted values and log probabilities
+        return V, log_probs
+    
+
+    def learn(self, total_timesteps):
+        t_so_far = 0 # timesteps so far
+        while t_so_far < total_timesteps:
+            ep_obs, ep_acts, ep_log_probs, ep_rtgs, ep_len = self.rollout()
+
+            # add how many timesteps were taken in the episode to the total so far
+            t_so_far += np.sum(ep_len)
+
+            # calcule V_{phi, k}
+            V, _ = self.evaluate(ep_obs, ep_acts)
+
+            #calculate advantage
+            A_k = ep_rtgs - V.detach()
+
+            # normalize advantages
+            A_k = (A_k - A_k.mean())/(A_k.std()*1e-10)
+
+            for _ in range(self.n_updates_per_iteration):
+                # calculate Pi_theta(a_t | s_t)
+                V, curr_log_probs = self.evaluate(ep_obs, ep_acts)
+
+                # calculate ratios
+                ratios = torch.exp(curr_log_probs - ep_log_probs)
+
+                # calculate surrogate losses
+                surr1 = ratios * A_k
+                surr2 = torch.clamp(ratios, 1 - self.clip, 1+ self.clip)
+
+                actor_loss = (-torch.min(surr1, surr2))
+                critic_loss = nn.MSELoss()(V, ep_rtgs)
+
+                # calculate gradients and perform backpropagation on the actor and critic networks
+                self.actor_optim.zero_grad()
+                actor_loss.backward(retain_graph=True)
+                self.actor_optim.step()
+
+                self.critic_optim.zero_grad()
+                critic_loss.backward()
+                self.critic_optim.step()
+            # Wait for the setup to be reset
+            input("Please reset setup and press Enter to continue")
+            input("Please press enter to confirm continuation")
+    
+num_tendons = 6
+num_encoders = 6
+model = singleFinger(num_tendons, num_encoders)
+model.learn(10000)
